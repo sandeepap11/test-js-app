c2b7b9355b744d629c49dd02e8b403ccf0dd4214
Sandeep Madavu Ramachandra <sandeepap11@gmail.com>
2024-09-07 11:37:37 +0530

    build fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,23 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./reset.css";
 import "./index.css";
-import * as Comlink from "comlink";
+//import * as Comlink from "comlink";
 
 //type WorkerModule = typeof import('./worker');
 
 // Create Worker
-const workerInstance = new ComlinkWorker(
-  new URL("./webworker/worker", import.meta.url)
-);
+// const workerInstance = new ComlinkWorker(
+//   new URL("./webworker/worker", import.meta.url)
+// );
 
-const result = await workerInstance.add(2, 3);
-console.log(result);
+// const result = await workerInstance.add(2, 3);
+// console.log(result);
 
-export const callback = (string) => console.log(string);
+// export const callback = (string) => console.log(string);
 
-const proxiedCallback = Comlink.proxy(callback);
+// const proxiedCallback = Comlink.proxy(callback);
 
-await workerInstance.executeCallback(proxiedCallback);
+// await workerInstance.executeCallback(proxiedCallback);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
